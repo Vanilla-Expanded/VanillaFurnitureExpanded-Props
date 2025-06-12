8fd15179ef69a35ea924b91a12b08a6ae61fbcf2
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-12 10:12:17 +0200

    v5.0

---
+++ 1.6/Source/VFEProps/VFEProps/Building/Building_FakeSteamGeyser.cs
@@ -0,0 +1,53 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.Sound;
+
+namespace VFEProps
+{
+    public class Building_FakeSteamGeyser : Building_SubstractsSilver
+    {
+        private IntermittentSteamSprayer steamSprayer;
+
+        private Sustainer spraySustainer;
+
+        private int spraySustainerStartTick = -999;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            steamSprayer = new IntermittentSteamSprayer(this);
+            steamSprayer.startSprayCallback = StartSpray;
+            steamSprayer.endSprayCallback = EndSpray;
+        }
+
+        private void StartSpray()
+        {
+         
+            spraySustainer = SoundDefOf.GeyserSpray.TrySpawnSustainer(new TargetInfo(base.Position, base.Map));
+            spraySustainerStartTick = Find.TickManager.TicksGame;
+        }
+
+        private void EndSpray()
+        {
+            if (spraySustainer != null)
+            {
+                spraySustainer.End();
+                spraySustainer = null;
+            }
+        }
+
+        protected override void Tick()
+        {
+            
+            steamSprayer.SteamSprayerTick();
+           
+            if (spraySustainer != null && Find.TickManager.TicksGame > spraySustainerStartTick + 1000)
+            {
+                Log.Message("Fake Geyser spray sustainer still playing after 1000 ticks. Force-ending.");
+                spraySustainer.End();
+                spraySustainer = null;
+            }
+        }
+    }
+}
---
+++ 1.6/Source/VFEProps/VFEProps/Comps/CompMoteReleaser.cs
@@ -0,0 +1,43 @@
+﻿
+using RimWorld;
+using UnityEngine;
+using Verse;
+namespace VFEProps
+{
+    [StaticConstructorOnStartup]
+    public class CompMoteReleaser : ThingComp
+    {
+
+        private Mote mote;
+
+        public CompProperties_MoteReleaser Props => (CompProperties_MoteReleaser)props;
+
+
+        public override void CompTick()
+        {
+            if (this.parent.Map != null)
+            {
+                if (mote == null)
+                {
+                    mote = MoteMaker.MakeStaticMote(parent.DrawPos, parent.Map, Props.moteDef);
+                    mote.instanceColor = parent.DrawColor;
+                }
+                if (mote.def.mote.needsMaintenance)
+                {
+                    mote.Maintain();
+                }
+
+            }
+        }
+
+        public override void Notify_ColorChanged()
+        {
+            mote = null;
+        }
+
+
+
+
+
+    }
+}
